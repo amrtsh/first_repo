4fcf11b4cd53e252b9c35adc0544734c4b3a7270
amrtsh <alina.vereshchak.ir.2022@lpnu.ua>
2022-11-28 13:38:17 +0200

    add cpp to branch

---
+++ main1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    float x = 2.361;
+    float y = 1.149;
+    double result = 13 * x * y + log(x / y) - 17 * sin(pow(x,2)-y);
+    printf("%f", result);
+
+    return 0;
+}//
+//
